796755b656d2d7fb69f554899d44a58d63b69eac
kalizink <kalizink65@gmail.com>
2024-01-26 11:23:25 -0600

    Integrated dictionary api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
 	let [keyword, setKeyword] = useState("");
 
-	function handleKeywordChange(event) {
-		setKeyword(event.target.value);
+	function handleResponse(response) {
+		console.log(response);
 	}
 
 	function search(event) {
 		event.preventDefault();
-		alert(`Searching for ${keyword}`);
+		let apiKey = "2ce1e1bf2899t12d0f6obada63d3f945";
+		let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+		axios.get(apiUrl).then(handleResponse);
+	}
+
+	function handleKeywordChange(event) {
+		setKeyword(event.target.value);
 	}
 
 	return (
