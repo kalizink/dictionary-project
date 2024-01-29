0b428d393efc5ef0d00fed044055c771e5ca983f
kalizink <kalizink65@gmail.com>
2024-01-29 10:08:48 -0600

    Edited footer links and added background color

--- src/Footer.js
+++ src/Footer.js
@@ -5,19 +5,23 @@ export default function Footer() {
 	return (
 		<div className="Footer ">
 			Coded by
-			<a href="https://github.com/kalizink" title="View profile">
+			<a href="https://github.com/kalizink" title="View profile" target="blank">
 				{" "}
 				Kali Zink{" "}
 			</a>
 			and is
 			<a
 				href="https://github.com/kalizink/dictionary-project.git"
-				title="View code">
+				title="View code"
+				target="blank">
 				{" "}
 				open sourced on GitHub{" "}
 			</a>
 			and
-			<a href="https://dictionary-kz.netlify.app/" title="Netlify">
+			<a
+				href="https://dictionary-kz.netlify.app/"
+				title="Netlify"
+				target="blank">
 				{" "}
 				hosted on Netlify
 			</a>
--- src/Results.js
+++ src/Results.js
@@ -3,7 +3,7 @@ import Meaning from "./Meaning.js";
 
 
 export default function Results(props) {
-	console.log(props.results);
+
 	if (props.results) {
 		return (
 			<div className="Results">
