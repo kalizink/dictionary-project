971380b27074cc1c4a340a3b155999e32bd25eee
kalizink <kalizink65@gmail.com>
2024-01-29 19:08:21 -0600

    Removed logs

--- src/Meaning.js
+++ src/Meaning.js
@@ -3,12 +3,9 @@ import "./Meaning.css";
 import Synonyms from "./Synonyms.js";
 
 export default function Meaning(props) {
-	console.log(props);
-	console.log(props.meaning.synonyms);
 	return (
 		<div className="Meaning">
 			<h3>{props.meaning.partOfSpeech}</h3>
-
 			<div className="definition">{props.meaning.definition}</div>
 			<div className="example">{props.meaning.example} </div>
 			<Synonyms synonyms={props.meaning.synonyms} />
